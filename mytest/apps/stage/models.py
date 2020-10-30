e3f6a482e02d8f73cf94c5c43999e54aa31c28c2
etteryand0 <etteryand@gmail.com>
2020-10-30 17:13:47 +0900

    database 3/10

--- mytest/apps/stage/models.py
+++ mytest/apps/stage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Stage(models.Model):
+    stage_opponent = models.CharField('Номер мошенника', max_length=11)
+    stage_time = models.CharField('Время инцидента', max_length=5)
+
+class Dialogue(models.Model):
+    dialogue_accept_reaction = models.CharField('Реакция на да', max_length=100)
+    dialogue_accept_score = models.IntegerField('Баллы за да')
+
+    dialogue_reaction = models.BooleanField('Реакция игрока')
+
+    dialogue_deny_reaction = models.CharField('Реакция побега', max_length=100)
+    dialogue_deny_score = models.IntegerField('Баллы за побег')
+
+class User(models.Model):
+    pass
+
--- mytest/urls.py
+++ mytest/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
+    path('stage/', include('stage.urls')),
     path('admin/', admin.site.urls),
 ]
